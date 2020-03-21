a6c81bcf3b2b4b64975895dc707cd77cf86822f7
Errol S <4250828+ksuclipse@users.noreply.github.com>
2020-03-21 13:21:00 -0600

    Platform Init

--- CabinetLights.ino
+++ CabinetLights.ino
@@ -9,7 +9,7 @@
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 #include <ESP8266mDNS.h>
 #include <ArduinoJson.h>
-#include <WebOTA.h>
+#include <ArduinoOTA.h>
 #include "config.h"
 
 
@@ -221,8 +221,55 @@ void setup() {
   mac.toCharArray(macAdd, 30);
 
 
-  //init webota
-  webota.init(8080, "/update");
+  //init OTA
+  // Port defaults to 8266
+  ArduinoOTA.setPort(8080);
+
+ // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("CabinetLights-L");
+
+ // No authentication by default
+  ArduinoOTA.setPassword("admin");
+
+ // Password can be set with it's md5 value as well
+ // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+ // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+ ArduinoOTA.onStart([]() {
+   String type;
+   if (ArduinoOTA.getCommand() == U_FLASH) {
+     type = "sketch";
+   } else { // U_FS
+     type = "filesystem";
+   }
+
+   // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+   Serial.println("Start updating " + type);
+ });
+ ArduinoOTA.onEnd([]() {
+   Serial.println("\nEnd");
+ });
+ ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+   Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+ });
+ ArduinoOTA.onError([](ota_error_t error) {
+   Serial.printf("Error[%u]: ", error);
+   if (error == OTA_AUTH_ERROR) {
+     Serial.println("Auth Failed");
+   } else if (error == OTA_BEGIN_ERROR) {
+     Serial.println("Begin Failed");
+   } else if (error == OTA_CONNECT_ERROR) {
+     Serial.println("Connect Failed");
+   } else if (error == OTA_RECEIVE_ERROR) {
+     Serial.println("Receive Failed");
+   } else if (error == OTA_END_ERROR) {
+     Serial.println("End Failed");
+   }
+ });
+ ArduinoOTA.begin();
+ Serial.println("Ready");
+ Serial.print("IP address: ");
+ Serial.println(WiFi.localIP());
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -241,7 +288,7 @@ void setup() {
   // set LWT
   baseTopic.toCharArray(statusTopic,30);
   strcat(statusTopic, "status");
-  
+
   // Initial MQTT Setup
   client.connect(clientName, MQTT_USER, MQTT_PASS, statusTopic, 1, 1, "offline");
   mqtt.publish(baseTopic + "status", "online", true);
@@ -279,7 +326,7 @@ void loop() {
 
 
 
-  webota.handle();
+  ArduinoOTA.handle();
   server.handleClient();
   MDNS.update();
   checkState();
